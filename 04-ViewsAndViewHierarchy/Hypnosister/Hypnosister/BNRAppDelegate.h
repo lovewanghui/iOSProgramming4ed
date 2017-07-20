9170a1ef6a0af038ebc8c37ce824d89837c409ee
DMH <allisman@163.com>
2017-07-20 18:03:27 +0800

    增加了UIView的一些注释.

--- 04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/BNRAppDelegate.h
+++ 04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/BNRAppDelegate.h
@@ -12,4 +12,8 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
+/**
+ 视图的层次结构:
+ 任何应用都只有一个UIWindow对象,它就像一个容器负责包含所有的视图.
+ */
 @end
--- 04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/BNRHypnosisView.m
+++ 04-ViewsAndViewHierarchy/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -10,6 +10,36 @@
 
 @implementation BNRHypnosisView
 
+/*
+ 该方法是UIView指定的初始化方法.
+其中参数所涉及的机构如下:
+ 
+ //Rectangles.
+struct CGRect {
+    CGPoint origin;
+    CGSize size;
+};
+typedef struct CGRect CGRect;
+ //Rectangle edges.
+
+//Points.
+
+struct CGPoint {
+    CGFloat x;
+    CGFloat y;
+};
+typedef struct CGPoint CGPoint;
+
+//Sizes.
+
+struct CGSize {
+    CGFloat width;
+    CGFloat height;
+};
+typedef struct CGSize CGSize;
+
+
+ */
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -22,6 +52,18 @@
 
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
+//在这里,关于drawRect:方法不必过多的关心,只需要知道视图根据该方法将自己绘制到图层cayer上.注意frame和bounds
+
+/**
+ @property(nonatomic,readonly,strong)                 CALayer  *layer;              // returns view's layer. Will always return a non-nil value. view is layer's delegate
+ 
+calayer和view的区别:
+ 
+ 1.首先UIView可以响应事件，Layer不可以.继承自不同的类导致的
+ 3.UIView主要是对显示内容的管理而 CALayer 主要侧重显示内容的绘制。
+ 每个 UIView 内部都有一个 CALayer 在背后提供内容的绘制和显示
+ 
+ */
 - (void)drawRect:(CGRect)rect
 {
     CGRect bounds = self.bounds;
